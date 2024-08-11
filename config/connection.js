9c714d163bea8173fd896f0ac80dd64fda42854f
Jocephuss <rowlandjosiah14@gmail.com>
2024-08-11 14:47:14 -0600

    'trying to get it deployed to render '

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,31 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-let sequelize;
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: "localhost",
+    dialect: "postgres",
+    port: 5432,
+  }
+);
 
-if (process.env.DB_URL) {
-  sequelize = new Sequelize(process.env.DB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PW,
-    {
-      host: "localhost",
-      dialect: "postgres",
-    }
-  );
-}
+// let sequelize;
+
+// if (process.env.DB_URL) {
+//   sequelize = new Sequelize(process.env.DB_URL);
+// } else {
+//   sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PW,
+//     {
+//       host: "localhost",
+//       dialect: "postgres",
+//     }
+//   );
+// }
 
 module.exports = sequelize;
