bd6b40bac68d5e9c10abeb1035649ca757a74552
Jocephuss <rowlandjosiah14@gmail.com>
2024-08-11 15:21:02 -0600

    'trying to get this deployed and the logout working'

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -54,7 +54,7 @@ router.post("/login", async (req, res) => {
 router.post("/logout", (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
-      res.status(204).res.redirect("/");
+      res.status(204).res.redirect("/login");
     });
   } else {
     res.status(404).end();
